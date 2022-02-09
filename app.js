b7d29d4e04c8f51a9acaf161b6c728c3da33209f
Ajr Wum <ajr.wum@gmail.com>
2022-02-09 17:35:36 +0100

    add necessary changes (app.js and dist added)  before deployment

--- app.js
+++ app.js
@@ -52,4 +52,17 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/foods', require('./routes/foods'));
 app.use('/meals', require('./routes/meals'));
 
+app.use('/api/*', (req, res, next) => {
+  const error = new Error('Ressource not found.');
+  error.status = 404;
+  next(error);
+});
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('*', (req, res, next) => {
+    // If no routes match, send them the React HTML.
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+  });
+}
+
 module.exports = app;
